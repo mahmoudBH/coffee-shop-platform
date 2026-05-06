50d08ba1133798ce41ddcb01c30f9cdd948acf61
mahmoudBH <boussbimahmoud@gmail.com>
2026-05-06 02:58:28 +0100

    feat: ajout du menu complet avec toutes les images statiques

--- client/src/Components/Food Menu/MenuData.js
+++ client/src/Components/Food Menu/MenuData.js
@@ -1,75 +1,121 @@
+// Coffees
 import coffee1 from '../../assests/menu/Coffee/coffee-1.png';
 import coffee2 from '../../assests/menu/Coffee/coffee-2.png';
+import coffee3 from '../../assests/menu/Coffee/coffee-3.png';
+import coffee4 from '../../assests/menu/Coffee/coffee-4.png';
+import coffee5 from '../../assests/menu/Coffee/coffee-5.png';
+import coffee6 from '../../assests/menu/Coffee/coffee-6.png';
+import coffee7 from '../../assests/menu/Coffee/coffee-7.png';
+import coffee8 from '../../assests/menu/Coffee/coffee-8.png';
+import coffee9 from '../../assests/menu/Coffee/coffee-9.png';
+import coffee10 from '../../assests/menu/Coffee/coffee-10.png';
+
+// Teas
 import tea1 from '../../assests/menu/Tea/tea-1.png';
+import tea2 from '../../assests/menu/Tea/tea-2.png';
+import tea3 from '../../assests/menu/Tea/tea-3.png';
+import tea4 from '../../assests/menu/Tea/tea-4.png';
+import tea5 from '../../assests/menu/Tea/tea-5.png';
+
+// Juices
 import juice1 from '../../assests/menu/Juices/juice-1.jpg';
+import juice2 from '../../assests/menu/Juices/juice-2.jpg';
+import juice3 from '../../assests/menu/Juices/juice-3.jpg';
+import juice4 from '../../assests/menu/Juices/juice-4.jpg';
+import juice5 from '../../assests/menu/Juices/juice-5.jpg';
+import juice6 from '../../assests/menu/Juices/juice-6.jpg';
+import juice7 from '../../assests/menu/Juices/juice-7.jpg';
+import juice8 from '../../assests/menu/Juices/juice-8.jpg';
+import juice9 from '../../assests/menu/Juices/juice-9.jpg';
+import juice10 from '../../assests/menu/Juices/juice-10.jpg';
+
+// Ice Cream
 import icecream1 from '../../assests/menu/Ice/icecream-1.jpg';
+import icecream2 from '../../assests/menu/Ice/icecream-2.jpg';
+import icecream3 from '../../assests/menu/Ice/icecream-3.jpg';
+
+// Pastries
 import pastries1 from '../../assests/menu/Pastries/pastries-1.jpg';
+import pastries2 from '../../assests/menu/Pastries/pastries-2.jpg';
+import pastries3 from '../../assests/menu/Pastries/pastries-3.jpg';
+import pastries4 from '../../assests/menu/Pastries/pastries-4.jpg';
+import pastries5 from '../../assests/menu/Pastries/pastries-5.jpg';
+import pastries6 from '../../assests/menu/Pastries/pastries-6.jpg';
+import pastries7 from '../../assests/menu/Pastries/pastries-7.jpg';
+import pastries8 from '../../assests/menu/Pastries/pastries-8.jpg';
+import pastries9 from '../../assests/menu/Pastries/pastries-9.jpg';
+
+// Crepes
 import crepes1 from '../../assests/menu/Crepes/crepes-1.jpg';
+import crepes2 from '../../assests/menu/Crepes/crepes-2.jpg';
+import crepes3 from '../../assests/menu/Crepes/crepes-3.jpg';
+import crepes4 from '../../assests/menu/Crepes/crepes-4.jpg';
+import crepes5 from '../../assests/menu/Crepes/crepes-5.jpg';
+import crepes6 from '../../assests/menu/Crepes/crepes-6.jpg';
+
+// Extras
 import extras1 from '../../assests/menu/Extras/extras-1.jpg';
+import extras2 from '../../assests/menu/Extras/extras-2.jpg';
+import extras3 from '../../assests/menu/Extras/extras-3.jpg';
 
 export const MenuData = [
-  {
-    id: 1,
-    title: "Classic Espresso",
-    category: "Coffee",
-    price: 3.5,
-    desc: "Un espresso riche et intense pour bien commencer la journée.",
-    img: coffee1
-  },
-  {
-    id: 2,
-    title: "Cappuccino Mousse",
-    category: "Coffee",
-    price: 5.0,
-    desc: "Un délicieux cappuccino avec une mousse de lait onctueuse.",
-    img: coffee2
-  },
-  {
-    id: 3,
-    title: "Thé Vert Menthe",
-    category: "Tea",
-    price: 4.0,
-    desc: "Thé vert infusé avec de la menthe fraîche.",
-    img: tea1
-  },
-  {
-    id: 4,
-    title: "Jus d'Orange Pressé",
-    category: "Juices & Cocktails",
-    price: 6.0,
-    desc: "100% pur jus d'oranges pressées.",
-    img: juice1
-  },
-  {
-    id: 5,
-    title: "Coupe Glacée Vanille",
-    category: "Ice Cream",
-    price: 7.5,
-    desc: "Trois boules de glace vanille avec coulis de chocolat.",
-    img: icecream1
-  },
-  {
-    id: 6,
-    title: "Croissant au Beurre",
-    category: "Pastries",
-    price: 2.5,
-    desc: "Un croissant frais et croustillant.",
-    img: pastries1
-  },
-  {
-    id: 7,
-    title: "Crêpe Nutella Banane",
-    category: "Crepes",
-    price: 6.5,
-    desc: "Une crêpe gourmande fourrée au Nutella et morceaux de banane.",
-    img: crepes1
-  },
-  {
-    id: 8,
-    title: "Extra Sirop Caramel",
-    category: "Extras",
-    price: 1.0,
-    desc: "Dose supplémentaire de sirop pour vos boissons.",
-    img: extras1
-  }
+  // --- COFFEES ---
+  { id: 1, title: "Espresso Classique", category: "Coffee", price: 2.5, desc: "Café court, intense et aromatique.", img: coffee1 },
+  { id: 2, title: "Cappuccino Mousse", category: "Coffee", price: 4.5, desc: "Espresso recouvert d'une mousse de lait onctueuse.", img: coffee2 },
+  { id: 3, title: "Latte Macchiato", category: "Coffee", price: 4.0, desc: "Grand verre de lait chaud, surmonté d'un espresso.", img: coffee3 },
+  { id: 4, title: "Café Américain", category: "Coffee", price: 3.0, desc: "Espresso allongé d'eau chaude.", img: coffee4 },
+  { id: 5, title: "Flat White", category: "Coffee", price: 4.5, desc: "Double espresso et lait micro-moussé.", img: coffee5 },
+  { id: 6, title: "Mocha Chocolat", category: "Coffee", price: 5.5, desc: "Mélange gourmand de café, lait et chocolat.", img: coffee6 },
+  { id: 7, title: "Iced Coffee", category: "Coffee", price: 4.5, desc: "Café glacé rafraîchissant pour l'été.", img: coffee7 },
+  { id: 8, title: "Caramel Macchiato", category: "Coffee", price: 5.5, desc: "Latte parfumé au sirop de caramel.", img: coffee8 },
+  { id: 9, title: "Café Turc", category: "Coffee", price: 3.5, desc: "Café traditionnel finement moulu et épicé.", img: coffee9 },
+  { id: 10, title: "Irish Coffee", category: "Coffee", price: 7.5, desc: "Café chaud, whisky irlandais et crème fouettée.", img: coffee10 },
+
+  // --- TEAS ---
+  { id: 11, title: "Thé à la Menthe", category: "Tea", price: 3.5, desc: "Thé vert traditionnel aux feuilles de menthe fraîche.", img: tea1 },
+  { id: 12, title: "Thé Noir Classique", category: "Tea", price: 3.0, desc: "Thé noir intense et réconfortant.", img: tea2 },
+  { id: 13, title: "Thé Vert Jasmin", category: "Tea", price: 4.0, desc: "Thé vert léger aux fleurs de jasmin.", img: tea3 },
+  { id: 14, title: "Infusion Camomille", category: "Tea", price: 3.5, desc: "Infusion douce et apaisante.", img: tea4 },
+  { id: 15, title: "Earl Grey", category: "Tea", price: 3.5, desc: "Thé noir parfumé à la bergamote.", img: tea5 },
+
+  // --- JUICES & COCKTAILS ---
+  { id: 16, title: "Jus d'Orange Pressé", category: "Juices & Cocktails", price: 5.0, desc: "Jus d'oranges fraîches pressées à la commande.", img: juice1 },
+  { id: 17, title: "Citronnade Fraîche", category: "Juices & Cocktails", price: 4.5, desc: "Citronnade maison désaltérante.", img: juice2 },
+  { id: 18, title: "Smoothie Fraise Banane", category: "Juices & Cocktails", price: 6.5, desc: "Mélange onctueux de fraises et de bananes.", img: juice3 },
+  { id: 19, title: "Cocktail Tropical", category: "Juices & Cocktails", price: 7.0, desc: "Mélange exotique mangue, ananas, passion.", img: juice4 },
+  { id: 20, title: "Detox Vert", category: "Juices & Cocktails", price: 6.0, desc: "Pomme, concombre, céleri et gingembre.", img: juice5 },
+  { id: 21, title: "Jus de Pomme", category: "Juices & Cocktails", price: 4.5, desc: "Pur jus de pomme.", img: juice6 },
+  { id: 22, title: "Mix Ananas Pêche", category: "Juices & Cocktails", price: 6.5, desc: "Cocktail fruité et sucré.", img: juice7 },
+  { id: 23, title: "Splash Pastèque", category: "Juices & Cocktails", price: 6.0, desc: "Jus très frais à la pastèque.", img: juice8 },
+  { id: 24, title: "Mojito Virgin", category: "Juices & Cocktails", price: 7.0, desc: "Menthe, citron vert, limonade et glace pilée.", img: juice9 },
+  { id: 25, title: "Fruits Rouges Blast", category: "Juices & Cocktails", price: 6.5, desc: "Explosion de saveurs aux baies rouges.", img: juice10 },
+
+  // --- ICE CREAM ---
+  { id: 26, title: "Coupe Vanille", category: "Ice Cream", price: 5.0, desc: "Deux boules de crème glacée à la vanille.", img: icecream1 },
+  { id: 27, title: "Chocolat Liégeois", category: "Ice Cream", price: 6.5, desc: "Glace chocolat, sauce cacao et chantilly.", img: icecream2 },
+  { id: 28, title: "Fraise Melba", category: "Ice Cream", price: 7.0, desc: "Glace vanille, fraises fraîches et coulis.", img: icecream3 },
+
+  // --- PASTRIES ---
+  { id: 29, title: "Croissant au Beurre", category: "Pastries", price: 2.0, desc: "Classique et croustillant.", img: pastries1 },
+  { id: 30, title: "Pain au Chocolat", category: "Pastries", price: 2.5, desc: "Viennoiserie fondante au cœur chocolat.", img: pastries2 },
+  { id: 31, title: "Muffin Myrtille", category: "Pastries", price: 3.5, desc: "Muffin moelleux aux myrtilles fraîches.", img: pastries3 },
+  { id: 32, title: "Brownie Noix de Pécan", category: "Pastries", price: 4.0, desc: "Gâteau chocolat dense et croquant.", img: pastries4 },
+  { id: 33, title: "Cheesecake Framboise", category: "Pastries", price: 5.5, desc: "Dessert onctueux au fromage frais.", img: pastries5 },
+  { id: 34, title: "Tartelette Citron", category: "Pastries", price: 4.5, desc: "Tarte acidulée au citron et meringue.", img: pastries6 },
+  { id: 35, title: "Roulé à la Cannelle", category: "Pastries", price: 3.5, desc: "Brioche roulée au sucre à la cannelle.", img: pastries7 },
+  { id: 36, title: "Assortiment Macarons", category: "Pastries", price: 6.0, desc: "Boîte de 3 macarons aux saveurs variées.", img: pastries8 },
+  { id: 37, title: "Éclair au Café", category: "Pastries", price: 4.0, desc: "Pâte à choux garnie de crème café.", img: pastries9 },
+
+  // --- CREPES ---
+  { id: 38, title: "Crêpe Nutella", category: "Crepes", price: 5.0, desc: "Crêpe tartinée généreusement au Nutella.", img: crepes1 },
+  { id: 39, title: "Crêpe Banane Chocolat", category: "Crepes", price: 6.5, desc: "Nutella et rondelles de banane.", img: crepes2 },
+  { id: 40, title: "Crêpe Fraise", category: "Crepes", price: 6.5, desc: "Crêpe fraîche aux fraises de saison.", img: crepes3 },
+  { id: 41, title: "Crêpe Caramel Beurre Salé", category: "Crepes", price: 5.5, desc: "Crêpe artisanale au caramel fondant.", img: crepes4 },
+  { id: 42, title: "Crêpe Sucre Citron", category: "Crepes", price: 4.0, desc: "Simple, légère et acidulée.", img: crepes5 },
+  { id: 43, title: "Crêpe Pistache", category: "Crepes", price: 7.0, desc: "Crêpe à la crème de pistache grillée.", img: crepes6 },
+
+  // --- EXTRAS ---
+  { id: 44, title: "Extra Chantilly", category: "Extras", price: 1.0, desc: "Pour plus de gourmandise.", img: extras1 },
+  { id: 45, title: "Coulis Miel", category: "Extras", price: 1.0, desc: "Idéal pour sucrer vos thés.", img: extras2 },
+  { id: 46, title: "Pépites de Chocolat", category: "Extras", price: 1.5, desc: "À parsemer sur vos boissons ou crêpes.", img: extras3 }
 ];
